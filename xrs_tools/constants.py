0a783a0b6bbd4adf7201ed56246e8a95df9dcaae
John ZuHone <jzuhone@gmail.com>
2016-08-15 18:21:27 -0400

    Grab constants from astropy

--- xrs_tools/constants.py
+++ xrs_tools/constants.py
@@ -1,4 +1,6 @@
-erg_per_eV = 1.602176562e-12
+import astropy.units as u
+
+erg_per_eV = u.eV.to("erg")
 erg_per_keV = erg_per_eV * 1.0e3
 keV_per_erg = 1.0 / erg_per_keV
 eV_per_erg = 1.0 / erg_per_eV
