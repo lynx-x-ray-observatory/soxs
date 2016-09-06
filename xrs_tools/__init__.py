caa62c60bce86c323cf007463b2e275cbb321934
John ZuHone <jzuhone@gmail.com>
2016-09-06 10:05:19 -0400

    Remove old reference

--- xrs_tools/__init__.py
+++ xrs_tools/__init__.py
@@ -1,5 +1,5 @@
 __version__ = "0.1.dev0"
 
 from xrs_tools.simput import write_simput_phlist
-from xrs_tools.spectra import Spectrum, PowerLawSpectrum
+from xrs_tools.spectra import Spectrum
 from xrs_tools.simx import run_simx
