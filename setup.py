e0809281bd481aa1ad62a12f6dd75c7b705475dc
John ZuHone <jzuhone@gmail.com>
2016-10-28 11:32:19 -0400

    Bumping the version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ cython_extensions = [
 
 setup(name='soxs',
       packages=['soxs'],
-      version="0.1",
+      version="0.1.1",
       description='Simulated Observations with X-ray Surveyor',
       author='John ZuHone',
       author_email='john.zuhone@cfa.harvard.edu',
--- soxs/__init__.py
+++ soxs/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.1"
+__version__ = "0.1.1"
 
 from soxs.simput import \
     write_photon_list, \
