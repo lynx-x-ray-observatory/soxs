e89aacff103a7fb5004d6aa3d32572df87b20122
John ZuHone <jzuhone@gmail.com>
2016-11-07 12:17:06 -0500

    Fix example

--- soxs/instrument.py
+++ soxs/instrument.py
@@ -238,7 +238,7 @@ def add_instrument_to_registry(inst_spec):
     ...     "name": "hdxi_3x10", # The short name of the instrument
     ...     "arf": "xrs_hdxi_3x10.arf", # The file containing the ARF
     ...     "rmf": "xrs_hdxi.rmf" # The file containing the RMF
-    ...     "bkgnd": "acisi_particle_bkgnd.dat" # The file containing the particle background
+    ...     "bkgnd": "acisi" # The name of the particle background
     ...     "plate_scale": 0.33333333333, # The plate scale in arcsec
     ...     "focal_length": 10.0, # The focal length in meters
     ...     "num_pixels": 4096, # The number of pixels on a side in the FOV
