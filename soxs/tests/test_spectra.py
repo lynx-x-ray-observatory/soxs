fa78029b1b5f57e13176e623d7f04d411a13449e
John ZuHone <jzuhone@gmail.com>
2016-11-25 00:32:19 -0500

    Read/write test

--- soxs/tests/test_spectra.py
+++ soxs/tests/test_spectra.py
@@ -1,5 +1,8 @@
 from soxs.spectra import Spectrum
 from numpy.testing import assert_allclose
+import os
+import tempfile
+import shutil
 
 def test_arithmetic():
     spec1 = Spectrum.from_powerlaw(1.0, 0.05, 1.0e-4)
@@ -21,3 +24,21 @@ def test_arithmetic():
     flux6 = spec3.flux/2.5
 
     assert_allclose(spec6.flux, flux6)
+
+def test_read_write():
+
+    tmpdir = tempfile.mkdtemp()
+    curdir = os.getcwd()
+    os.chdir(tmpdir)
+
+    spec1 = Spectrum.from_powerlaw(1.0, 0.05, 1.0e-4)
+    spec1.write_file("test_spec.dat", clobber=True)
+    spec2 = Spectrum.from_file("test_spec.dat")
+
+    assert_allclose(spec1.flux, spec2.flux)
+    assert_allclose(spec1.emid, spec2.emid)
+    assert_allclose(spec1.ebins, spec2.ebins)
+    assert_allclose(spec1.cumspec, spec2.cumspec)
+
+    os.chdir(curdir)
+    shutil.rmtree(tmpdir)
