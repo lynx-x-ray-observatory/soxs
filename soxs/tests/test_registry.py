b77210b4dc2e1af0dbaabb247e06cf70a84c47c4
John ZuHone <jzuhone@gmail.com>
2016-12-20 16:46:23 -0500

    Fix registry test

--- soxs/tests/test_registry.py
+++ soxs/tests/test_registry.py
@@ -1,7 +1,7 @@
 from soxs.instrument_registry import show_instrument_registry
-from soxs.background import show_background_registry
+from soxs.background.instrument import show_instrument_backgrounds
 
 def test_show():
     # Just make sure these don't throw any errors
-    show_background_registry()
+    show_instrument_backgrounds()
     show_instrument_registry()
