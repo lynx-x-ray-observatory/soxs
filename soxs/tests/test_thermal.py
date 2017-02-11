8a2757f00e4559c2c85ac1c68086aaea10cfda02
John ZuHone <jzuhone@gmail.com>
2017-02-11 16:34:32 -0500

    Add a test for simulate_spectrum

--- soxs/tests/test_thermal.py
+++ soxs/tests/test_thermal.py
@@ -8,7 +8,8 @@ from soxs.simput import write_photon_list
 from soxs.instrument_registry import \
     get_instrument_from_registry
 from soxs.instrument import instrument_simulator, \
-    RedistributionMatrixFile, AuxiliaryResponseFile
+    RedistributionMatrixFile, AuxiliaryResponseFile, \
+    simulate_spectrum
 from soxs.utils import convert_rmf
 from soxs.events import write_spectrum
 from sherpa.astro.ui import load_user_model, add_user_pars, \
@@ -30,23 +31,24 @@ def mymodel(pars, x, xhi=None):
     eidxs = np.logical_and(rmf.elo >= x[0]-0.5*dx, rmf.elo <= x[-1]+0.5*dx)
     return dx*wabs*apec.flux.value[eidxs]
 
+nH_sim = 0.02
+kT_sim = 6.0
+abund_sim = 0.4
+norm_sim = 1.0e-3
+redshift = 0.05
+
+exp_time = 5.0e4
+area = 40000.0
+
+spec = agen.get_spectrum(kT_sim, abund_sim, redshift, norm_sim)
+spec.apply_foreground_absorption(nH_sim)
+
 def test_thermal():
 
     tmpdir = tempfile.mkdtemp()
     curdir = os.getcwd()
     os.chdir(tmpdir)
 
-    nH_sim = 0.02
-    kT_sim = 6.0
-    abund_sim = 0.4
-    norm_sim = 1.0e-3
-    redshift = 0.05
-
-    exp_time = 5.0e4
-    area = 40000.0
-
-    spec = agen.get_spectrum(kT_sim, abund_sim, redshift, norm_sim)
-    spec.apply_foreground_absorption(nH_sim)
     e = spec.generate_energies(exp_time, area, prng=prng)
 
     pt_src = PointSourceModel(30.0, 45.0, e.size)
@@ -91,5 +93,47 @@ def test_thermal():
     os.chdir(curdir)
     shutil.rmtree(tmpdir)
 
+def test_thermal_from_spectrum():
+
+    tmpdir = tempfile.mkdtemp()
+    curdir = os.getcwd()
+    os.chdir(tmpdir)
+
+    inst = get_instrument_from_registry(inst_name)
+
+    simulate_spectrum(spec, inst["arf"], inst["rmf"], 
+                      exp_time, "thermal_model_evt.pha")
+
+    arf = AuxiliaryResponseFile(inst["arf"])
+    rmf = RedistributionMatrixFile(inst["rmf"])
+    os.system("cp %s ." % arf.filename)
+    convert_rmf(rmf.filename)
+
+    load_user_model(mymodel, "wapec")
+    add_user_pars("wapec", ["nH", "kT", "abund", "redshift", "norm"],
+                  [0.01, 4.0, 0.2, redshift, norm_sim*0.8],
+                  parmins=[0.0, 0.1, 0.0, -20.0, 0.0],
+                  parmaxs=[10.0, 20.0, 10.0, 20.0, 1.0e9],
+                  parfrozen=[False, False, False, True, False])
+
+    load_pha("thermal_model_evt.pha")
+    set_stat("cstat")
+    set_method("simplex")
+    set_model("wapec")
+    ignore(":0.5, 8.0:")
+    fit()
+    set_covar_opt("sigma", 1.645)
+    covar()
+    res = get_covar_results()
+
+    assert np.abs(res.parvals[0]-nH_sim) < res.parmaxes[0]
+    assert np.abs(res.parvals[1]-kT_sim) < res.parmaxes[1]
+    assert np.abs(res.parvals[2]-abund_sim) < res.parmaxes[2]
+    assert np.abs(res.parvals[3]-norm_sim) < res.parmaxes[3]
+
+    os.chdir(curdir)
+    shutil.rmtree(tmpdir)
+
 if __name__ == "__main__":
-    test_thermal()
+    #test_thermal()
+    test_thermal_from_spectrum()
