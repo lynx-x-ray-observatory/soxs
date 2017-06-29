2b3944e3d93fd24fd474b01a761e60f2e1a575a0
John ZuHone <jzuhone@gmail.com>
2017-06-29 13:50:28 -0400

    New spectrum test

--- soxs/tests/test_thermal.py
+++ soxs/tests/test_thermal.py
@@ -21,6 +21,10 @@ inst_name = "mucal"
 
 rmf = RedistributionMatrixFile("xrs_%s.rmf" % inst_name)
 agen = ApecGenerator(rmf.elo[0], rmf.ehi[-1], rmf.n_de, broadening=True)
+agen_var = ApecGenerator(rmf.elo[0], rmf.ehi[-1], rmf.n_de, 
+                         var_elem=["O", "Ca"], broadening=True)
+agen_nolines = ApecGenerator(rmf.elo[0], rmf.ehi[-1], rmf.n_de, 
+                             broadening=True, nolines=True)
 
 def mymodel(pars, x, xhi=None):
     dx = x[1]-x[0]
@@ -29,17 +33,41 @@ def mymodel(pars, x, xhi=None):
     eidxs = np.logical_and(rmf.elo >= x[0]-0.5*dx, rmf.elo <= x[-1]+0.5*dx)
     return dx*wabs*apec.flux.value[eidxs]
 
+def mymodel_var(pars, x, xhi=None):
+    dx = x[1]-x[0]
+    wabs = get_wabs_absorb(x+0.5*dx, pars[0])
+    apec = agen_var.get_spectrum(pars[1], pars[2], pars[3], pars[4],
+                                 elem_abund={"O": pars[5], "Ca": pars[6]})
+    eidxs = np.logical_and(rmf.elo >= x[0]-0.5*dx, rmf.elo <= x[-1]+0.5*dx)
+    return dx*wabs*apec.flux.value[eidxs]
+
+def mymodel_nolines(pars, x, xhi=None):
+    dx = x[1]-x[0]
+    wabs = get_wabs_absorb(x+0.5*dx, pars[0])
+    apec = agen_nolines.get_spectrum(pars[1], pars[2], pars[3], pars[4])
+    eidxs = np.logical_and(rmf.elo >= x[0]-0.5*dx, rmf.elo <= x[-1]+0.5*dx)
+    return dx*wabs*apec.flux.value[eidxs]
+
 nH_sim = 0.02
 kT_sim = 6.0
 abund_sim = 0.4
 norm_sim = 1.0e-3
 redshift = 0.05
+O_sim = 0.3
+Ca_sim = 0.5
 
 exp_time = 5.0e4
 area = 40000.0
 
 spec = agen.get_spectrum(kT_sim, abund_sim, redshift, norm_sim)
 spec.apply_foreground_absorption(nH_sim)
+
+spec_var = agen_var.get_spectrum(kT_sim, abund_sim, redshift, norm_sim,
+                                 elem_abund={"O": O_sim, "Ca": Ca_sim})
+spec_var.apply_foreground_absorption(nH_sim)
+
+spec_nolines = agen_nolines.get_spectrum(kT_sim, abund_sim, redshift, norm_sim)
+spec_nolines.apply_foreground_absorption(nH_sim)
 
 def test_thermal():
 
@@ -136,6 +164,95 @@ def test_thermal_from_spectrum():
     os.chdir(curdir)
     shutil.rmtree(tmpdir)
 
+def test_var_thermal_from_spectrum():
+
+    prng = RandomState(65)
+
+    tmpdir = tempfile.mkdtemp()
+    curdir = os.getcwd()
+    os.chdir(tmpdir)
+
+    inst = get_instrument_from_registry(inst_name)
+
+    simulate_spectrum(spec_var, inst["name"], exp_time,
+                      "var_thermal_model_evt.pha", prng=prng)
+
+    arf = AuxiliaryResponseFile(inst["arf"])
+    rmf = RedistributionMatrixFile(inst["rmf"])
+    os.system("cp %s ." % arf.filename)
+    convert_rmf(rmf.filename)
+
+    load_user_model(mymodel_var, "wapec")
+    add_user_pars("wapec", ["nH", "kT", "abund", "redshift", "norm", "O", "Ca"],
+                  [nH_sim, 4.0, 0.2, redshift, norm_sim*0.8, 0.5, 0.3],
+                  parmins=[0.0, 0.1, 0.0, -20.0, 0.0, 0.0, 0.0],
+                  parmaxs=[10.0, 20.0, 10.0, 20.0, 1.0e9, 10.0, 10.0],
+                  parfrozen=[True, False, False, True, False, False, False])
+
+    load_pha("var_thermal_model_evt.pha")
+    set_stat("cstat")
+    set_method("simplex")
+    set_model("wapec")
+    ignore(":0.5, 8.0:")
+    fit()
+    set_covar_opt("sigma", 1.645)
+    covar()
+    res = get_covar_results()
+
+    assert np.abs(res.parvals[0]-kT_sim) < res.parmaxes[0]
+    assert np.abs(res.parvals[1]-abund_sim) < res.parmaxes[1]
+    assert np.abs(res.parvals[2]-norm_sim) < res.parmaxes[2]
+    assert np.abs(res.parvals[3]-O_sim) < res.parmaxes[3]
+    assert np.abs(res.parvals[4]-Ca_sim) < res.parmaxes[4]
+
+    os.chdir(curdir)
+    shutil.rmtree(tmpdir)
+
+def test_nolines_thermal_from_spectrum():
+
+    prng = RandomState(65)
+
+    tmpdir = tempfile.mkdtemp()
+    curdir = os.getcwd()
+    os.chdir(tmpdir)
+
+    inst = get_instrument_from_registry(inst_name)
+
+    simulate_spectrum(spec_nolines, inst["name"], exp_time,
+                      "nolines_thermal_model_evt.pha", prng=prng)
+
+    arf = AuxiliaryResponseFile(inst["arf"])
+    rmf = RedistributionMatrixFile(inst["rmf"])
+    os.system("cp %s ." % arf.filename)
+    convert_rmf(rmf.filename)
+
+    load_user_model(mymodel_nolines, "wapec")
+    add_user_pars("wapec", ["nH", "kT", "abund", "redshift", "norm"],
+                  [0.01, 4.0, 0.2, redshift, norm_sim*0.8],
+                  parmins=[0.0, 0.1, 0.0, -20.0, 0.0],
+                  parmaxs=[10.0, 20.0, 10.0, 20.0, 1.0e9],
+                  parfrozen=[False, False, False, True, False])
+
+    load_pha("nolines_thermal_model_evt.pha")
+    set_stat("cstat")
+    set_method("simplex")
+    set_model("wapec")
+    ignore(":0.5, 8.0:")
+    fit()
+    set_covar_opt("sigma", 1.645)
+    covar()
+    res = get_covar_results()
+
+    assert np.abs(res.parvals[0]-nH_sim) < res.parmaxes[0]
+    assert np.abs(res.parvals[1]-kT_sim) < res.parmaxes[1]
+    assert np.abs(res.parvals[2]-abund_sim) < res.parmaxes[2]
+    assert np.abs(res.parvals[3]-norm_sim) < res.parmaxes[3]
+
+    os.chdir(curdir)
+    shutil.rmtree(tmpdir)
+
 if __name__ == "__main__":
     test_thermal()
     test_thermal_from_spectrum()
+    test_var_thermal_from_spectrum()
+    test_nolines_thermal_from_spectrum()
