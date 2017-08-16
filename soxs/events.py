b934130a5449adc42b99564b9b66148bb62fa406
John ZuHone <jzuhone@gmail.com>
2017-08-16 17:55:05 -0400

    unused import

--- soxs/events.py
+++ soxs/events.py
@@ -3,7 +3,7 @@ import astropy.io.fits as pyfits
 import astropy.wcs as pywcs
 import os
 from six import string_types
-from soxs.utils import mylog, parse_value, iterable, get_rot_mat
+from soxs.utils import mylog, parse_value, get_rot_mat
 from soxs.instrument_registry import instrument_registry
 from tqdm import tqdm
 
