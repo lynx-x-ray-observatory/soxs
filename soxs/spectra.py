924fc704e1fd3da4e06a0a6c62ec3f02d2db6e69
John ZuHone <jzuhone@gmail.com>
2018-02-26 15:27:54 -0500

    Bugfix

--- soxs/spectra.py
+++ soxs/spectra.py
@@ -732,7 +732,7 @@ class ApecGenerator(object):
             if len(abund_table) != 30:
                 raise RuntimeError("User-supplied abundance tables "
                                    "must be 30 elements long!")
-            self.atable = np.concatenate([0.0], np.array(abund_table))
+            self.atable = np.concatenate([[0.0], np.array(abund_table)])
         else:
             self.atable = abund_tables[abund_table].copy()
         self.atable[1:] /= abund_tables["angr"][1:]
