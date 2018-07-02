21bd07e0671a6bf289f145929f2323f73aff7b31
John ZuHone <jzuhone@gmail.com>
2018-07-02 08:57:43 -0400

    Some ansewr tests

--- soxs/tests/test_lines.py
+++ soxs/tests/test_lines.py
@@ -12,7 +12,8 @@ from numpy.random import RandomState
 
 prng = RandomState(69)
 
-def test_emission_line(generate):
+
+def test_emission_line(answer_store):
     tmpdir = tempfile.mkdtemp()
     curdir = os.getcwd()
     os.chdir(tmpdir)
@@ -29,29 +30,29 @@ def test_emission_line(generate):
     spec = Spectrum.from_constant(const_flux, 1.0, 10.0, 20000)
     spec.add_emission_line(line_pos, line_width, line_amp)
 
-    spectrum_answer_testing(spec, "emission_line_test.h5", generate)
+    spectrum_answer_testing(spec, "emission_line_test.h5", answer_store)
 
     pt_src_pos = PointSourceModel(30.0, 45.0)
-    sim_cat = SimputCatalog.from_models("emission_line", "emission_line", spec, pt_src_pos,
-                                        exp_time, area, prng=prng)
+    sim_cat = SimputCatalog.from_models("emission_line", "emission_line", spec, 
+                                        pt_src_pos, exp_time, area, prng=prng)
     sim_cat.write_catalog(overwrite=True)
 
-    instrument_simulator("emission_line_simput.fits", "emission_line_evt.fits", exp_time,
-                         inst_name, [30.0, 45.0], instr_bkgnd=False, ptsrc_bkgnd=False,
-                         foreground=False, prng=prng)
+    instrument_simulator("emission_line_simput.fits", "emission_line_evt.fits",
+                         exp_time, inst_name, [30.0, 45.0], instr_bkgnd=False,
+                         ptsrc_bkgnd=False, foreground=False, prng=prng)
 
-    file_answer_testing("EVENTS", "emission_line_evt_test.fits", 
-                        "emission_line_evt.fits", generate)
+    file_answer_testing("EVENTS", "emission_line_evt.fits", answer_store)
 
-    write_spectrum("emission_line_evt.fits", "emission_line_evt.pha", overwrite=True)
+    write_spectrum("emission_line_evt.fits", "emission_line_evt.pha",
+                   overwrite=True)
 
-    file_answer_testing("SPECTRUM", "emission_line_evt_test.pha", 
-                        "emission_line_evt.pha", generate)
+    file_answer_testing("SPECTRUM", "emission_line_evt.pha", answer_store)
 
     os.chdir(curdir)
     shutil.rmtree(tmpdir)
 
-def test_absorption_line(generate):
+
+def test_absorption_line(answer_store):
     tmpdir = tempfile.mkdtemp()
     curdir = os.getcwd()
     os.chdir(tmpdir)
@@ -68,30 +69,30 @@ def test_absorption_line(generate):
     spec = Spectrum.from_constant(const_flux, 0.1, 3.0, 100000)
     spec.add_absorption_line(line_pos, line_width, line_amp)
 
-    spectrum_answer_testing(spec, "absoprtion_line_test.h5", generate)
+    spectrum_answer_testing(spec, "absoprtion_line_test.h5", answer_store)
 
     pt_src_pos = PointSourceModel(30.0, 45.0)
-    sim_cat = SimputCatalog.from_models("absorption_line", "absorption_line", spec, pt_src_pos,
-                                        exp_time, area, prng=prng)
+    sim_cat = SimputCatalog.from_models("absorption_line", "absorption_line",
+                                        spec, pt_src_pos, exp_time, area, prng=prng)
     sim_cat.write_catalog(overwrite=True)
 
-    instrument_simulator("absorption_line_simput.fits", "absorption_line_evt.fits", exp_time,
-                         inst_name, [30.0, 45.0], instr_bkgnd=False, ptsrc_bkgnd=False,
-                         foreground=False, prng=prng)
+    instrument_simulator("absorption_line_simput.fits", "absorption_line_evt.fits",
+                         exp_time, inst_name, [30.0, 45.0], instr_bkgnd=False,
+                         ptsrc_bkgnd=False, foreground=False, prng=prng)
 
-    file_answer_testing("EVENTS", "absorption_line_evt_test.fits",
-                        "absorption_line_evt.fits", generate)
+    file_answer_testing("EVENTS", "absorption_line_evt.fits", answer_store)
 
-    write_spectrum("absorption_line_evt.fits", "absorption_line_evt.pha", overwrite=True)
+    write_spectrum("absorption_line_evt.fits", "absorption_line_evt.pha",
+                   overwrite=True)
 
-    file_answer_testing("SPECTRUM", "absorption_line_evt_test.pha",
-                        "absorption_line_evt.pha", generate)
+    file_answer_testing("SPECTRUM", "absorption_line_evt.pha", answer_store)
 
     os.chdir(curdir)
     shutil.rmtree(tmpdir)
 
+
 if __name__ == "__main__":
     import sys
-    generate = bool(sys.argv[1])
-    test_emission_line(generate)
-    test_absorption_line(generate)
+    answer_store = bool(sys.argv[1])
+    test_emission_line(answer_store)
+    test_absorption_line(answer_store)
