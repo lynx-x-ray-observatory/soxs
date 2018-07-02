be9301a29074d0c887089e2e01e5914956062431
John ZuHone <jzuhone@gmail.com>
2018-07-02 08:57:31 -0400

    File answer testing function

--- soxs/tests/utils.py
+++ soxs/tests/utils.py
@@ -2,6 +2,7 @@ import os
 from numpy.testing import assert_equal
 from astropy.io import fits
 from soxs.utils import soxs_cfg
+import shutil
 
 def get_test_path():
     test_path = soxs_cfg.get("soxs", "test_outputs_path")
@@ -9,6 +10,7 @@ def get_test_path():
         raise IOError("The SOXS test outputs directory "
                       "%s does not exist!" % test_path)
     return test_path
+
 
 def spectrum_answer_testing(spec, filename, generate):
     test_path = get_test_path()
@@ -21,14 +23,19 @@ def spectrum_answer_testing(spec, filename, generate):
         assert_equal(answer_spec.flux.value, spec.flux.value)
         assert answer_spec.flux.units == spec.flux.units
 
-def file_answer_testing(file_type, old_file, new_file, generate):
+
+def file_answer_testing(hdu, filename, generate):
     test_path = get_test_path()
-    oldf = os.path.join(test_path, old_file)
+    oldf = os.path.join(test_path, filename)
     if generate:
-        
+        shutil.copy(filename, test_path)
     else:
         f_old = fits.open(oldf)
-        f_new = fits.open(new_file)
-        
+        f_new = fits.open(filename)
+        old_cols = f_old[hdu].data.names
+        new_cols = f_new[hdu].data.names
+        assert old_cols == new_cols
+        for name in old_cols:
+            assert_equal(f_old[hdu].data[name], f_old[hdu].data[name])
         f_old.close()
         f_new.close()
