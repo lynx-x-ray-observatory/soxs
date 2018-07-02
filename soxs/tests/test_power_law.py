68f3a15a80e940b38c56cf7793c36f02b82312fc
John ZuHone <jzuhone@gmail.com>
2018-07-02 10:51:58 -0400

    Update these tests

--- soxs/tests/test_power_law.py
+++ soxs/tests/test_power_law.py
@@ -1,4 +1,3 @@
-import numpy as np
 import os
 import shutil
 import tempfile
@@ -6,20 +5,17 @@ from soxs.spectra import Spectrum, get_tbabs_absorb
 from soxs.spatial import PointSourceModel
 from soxs.simput import SimputCatalog
 from soxs.instrument_registry import \
-    get_instrument_from_registry, \
     make_simple_instrument
-from soxs.instrument import instrument_simulator, \
-    RedistributionMatrixFile, AuxiliaryResponseFile
-from soxs.utils import convert_rmf
+from soxs.instrument import instrument_simulator
 from soxs.events import write_spectrum
-from sherpa.astro.ui import load_user_model, add_user_pars, \
-    load_pha, ignore, fit, set_model, set_stat, set_method, \
-    covar, get_covar_results, set_covar_opt
 from numpy.random import RandomState
+from soxs.tests.utils import spectrum_answer_testing, \
+    file_answer_testing
 
 make_simple_instrument("hdxi", "new_hdxi", 20.0, 1024)
 
 prng = RandomState(69)
+
 
 def mymodel(pars, x, xhi=None):
     dx = x[1]-x[0]
@@ -27,12 +23,14 @@ def mymodel(pars, x, xhi=None):
     plaw = pars[1]*dx*(x*(1.0+pars[2]))**(-pars[3])
     return tbabs*plaw
 
-def test_power_law():
-    plaw_fit(1.1)
-    plaw_fit(0.8)
-    plaw_fit(1.0)
 
-def plaw_fit(alpha_sim):
+def test_power_law(answer_store):
+    plaw_fit(1.1, answer_store)
+    plaw_fit(0.8, answer_store)
+    plaw_fit(1.0, answer_store)
+
+
+def plaw_fit(alpha_sim, answer_store):
 
     tmpdir = tempfile.mkdtemp()
     curdir = os.getcwd()
@@ -49,46 +47,31 @@ def plaw_fit(alpha_sim):
     spec = Spectrum.from_powerlaw(alpha_sim, redshift, norm_sim, 0.1, 10.0, 20000)
     spec.apply_foreground_absorption(nH_sim, model="tbabs")
 
+    spectrum_answer_testing(spec, "power_law_%s.h5" % alpha_sim, answer_store)
+
     pt_src_pos = PointSourceModel(30.0, 45.0)
     sim_cat = SimputCatalog.from_models("plaw_model", "plaw_model", spec, pt_src_pos,
                                         exp_time, area, prng=prng)
     sim_cat.write_catalog(overwrite=True)
 
-    instrument_simulator("plaw_model_simput.fits", "plaw_model_evt.fits", exp_time, 
-                         inst_name, [30.0, 45.0], instr_bkgnd=False, ptsrc_bkgnd=False,
-                         foreground=False, prng=prng)
+    instrument_simulator("plaw_model_simput.fits", "plaw_model_%s_evt.fits" % alpha_sim,
+                         exp_time, inst_name, [30.0, 45.0], instr_bkgnd=False,
+                         ptsrc_bkgnd=False, foreground=False, prng=prng)
 
-    inst = get_instrument_from_registry(inst_name)
-    arf = AuxiliaryResponseFile(inst["arf"])
-    rmf = RedistributionMatrixFile(inst["rmf"])
-    os.system("cp %s ." % arf.filename)
-    convert_rmf(rmf.filename)
+    write_spectrum("plaw_model_%s_evt.fits" % alpha_sim,
+                   "plaw_model_%s_evt.pha" % alpha_sim,
+                   overwrite=True)
 
-    write_spectrum("plaw_model_evt.fits", "plaw_model_evt.pha", overwrite=True)
-
-    load_user_model(mymodel, "tplaw")
-    add_user_pars("tplaw", ["nH", "norm", "redshift", "alpha"],
-                  [0.01, norm_sim*0.8, redshift, 0.9], 
-                  parmins=[0.0, 0.0, 0.0, 0.1],
-                  parmaxs=[10.0, 1.0e9, 10.0, 10.0],
-                  parfrozen=[False, False, True, False])
-
-    load_pha("plaw_model_evt.pha")
-    set_stat("cstat")
-    set_method("simplex")
-    ignore(":0.6, 8.0:")
-    set_model("tplaw")
-    fit()
-    set_covar_opt("sigma", 1.645)
-    covar()
-    res = get_covar_results()
-
-    assert np.abs(res.parvals[0]-nH_sim) < res.parmaxes[0]
-    assert np.abs(res.parvals[1]-norm_sim) < res.parmaxes[1]
-    assert np.abs(res.parvals[2]-alpha_sim) < res.parmaxes[2]
+    file_answer_testing("EVENTS", "plaw_model_%s_evt.fits" % alpha_sim,
+                        answer_store)
+    file_answer_testing("SPECTRUM", "plaw_model_%s_evt.pha" % alpha_sim,
+                        answer_store)
 
     os.chdir(curdir)
     shutil.rmtree(tmpdir)
 
+
 if __name__ == "__main__":
-    test_power_law()
+    import sys
+    answer_store = bool(sys.argv[1])
+    test_power_law(answer_store)
