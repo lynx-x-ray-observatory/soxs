17ebb4b2e808b19be044c9f89f2785e443511b3d
John ZuHone <jzuhone@gmail.com>
2018-07-02 10:32:01 -0400

    Make this test a little more lightweight

--- soxs/tests/test_lines.py
+++ soxs/tests/test_lines.py
@@ -1,8 +1,9 @@
 import tempfile
 import shutil
-import os 
+import os
 from soxs.events import write_spectrum
-from soxs.instrument import instrument_simulator
+from soxs.instrument import instrument_simulator, \
+    simulate_spectrum
 from soxs.spatial import PointSourceModel
 from soxs.simput import SimputCatalog
 from soxs.spectra import Spectrum
@@ -23,8 +24,8 @@ def test_emission_line(answer_store):
     line_width = 0.02
     line_amp = 1.0e-5
 
-    exp_time = (500.0, "ks")
-    area = 40000.0
+    exp_time = (100.0, "ks")
+    area = 30000.0
     inst_name = "mucal"
 
     spec = Spectrum.from_constant(const_flux, 1.0, 10.0, 20000)
@@ -57,33 +58,21 @@ def test_absorption_line(answer_store):
     curdir = os.getcwd()
     os.chdir(tmpdir)
 
-    const_flux = 1.0e-2
+    const_flux = 1.0e-3
     line_pos = 1.0
     line_width = 0.02
     line_amp = 1.0e-5
 
-    exp_time = (500.0, "ks")
-    area = 40000.0
+    exp_time = (100.0, "ks")
     inst_name = "lynx_gratings"
 
     spec = Spectrum.from_constant(const_flux, 0.1, 3.0, 100000)
     spec.add_absorption_line(line_pos, line_width, line_amp)
 
-    spectrum_answer_testing(spec, "absoprtion_line_test.h5", answer_store)
+    spectrum_answer_testing(spec, "absorption_line_test.h5", answer_store)
 
-    pt_src_pos = PointSourceModel(30.0, 45.0)
-    sim_cat = SimputCatalog.from_models("absorption_line", "absorption_line",
-                                        spec, pt_src_pos, exp_time, area, prng=prng)
-    sim_cat.write_catalog(overwrite=True)
-
-    instrument_simulator("absorption_line_simput.fits", "absorption_line_evt.fits",
-                         exp_time, inst_name, [30.0, 45.0], instr_bkgnd=False,
-                         ptsrc_bkgnd=False, foreground=False, prng=prng)
-
-    file_answer_testing("EVENTS", "absorption_line_evt.fits", answer_store)
-
-    write_spectrum("absorption_line_evt.fits", "absorption_line_evt.pha",
-                   overwrite=True)
+    simulate_spectrum(spec, inst_name, exp_time, "absorption_line_evt.pha",
+                      overwrite=True)
 
     file_answer_testing("SPECTRUM", "absorption_line_evt.pha", answer_store)
 
