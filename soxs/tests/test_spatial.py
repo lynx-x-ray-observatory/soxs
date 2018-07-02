a09567d3afa216b3c025cf3756e762f05662dace
John ZuHone <jzuhone@gmail.com>
2018-07-02 10:39:40 -0400

    Answer testing for spatial

--- soxs/tests/test_spatial.py
+++ soxs/tests/test_spatial.py
@@ -1,6 +1,6 @@
 from soxs.spatial import PointSourceModel, BetaModel, \
     AnnulusModel
-from soxs.spectra import ApecGenerator, ConvolvedSpectrum
+from soxs.spectra import ApecGenerator
 import numpy as np
 import os
 import shutil
@@ -9,13 +9,10 @@ import astropy.io.fits as pyfits
 from astropy.units import Quantity
 from soxs.events import write_radial_profile, make_exposure_map
 from soxs.simput import SimputCatalog
-from soxs.instrument import instrument_simulator, sigma_to_fwhm, \
-    AuxiliaryResponseFile
+from soxs.instrument import instrument_simulator, sigma_to_fwhm
 from soxs.instrument_registry import get_instrument_from_registry, \
     add_instrument_to_registry
-from sherpa.astro.ui import set_source, freeze, \
-    fit, covar, get_covar_results, set_covar_opt, \
-    load_data, set_stat, set_method
+from soxs.tests.utils import file_answer_testing
 
 kT = Quantity(6.0, "keV")
 Z = 0.3
@@ -33,6 +30,7 @@ spec.apply_foreground_absorption(nH)
 
 ra0 = 30.0
 dec0 = 45.0
+
 
 def test_point_source():
     tmpdir = tempfile.mkdtemp()
@@ -71,7 +69,8 @@ def test_point_source():
     os.chdir(curdir)
     shutil.rmtree(tmpdir)
 
-def test_annulus():
+
+def test_annulus(answer_store):
 
     tmpdir = tempfile.mkdtemp()
     curdir = os.getcwd()
@@ -90,33 +89,18 @@ def test_annulus():
                          "hdxi", [ra0, dec0], ptsrc_bkgnd=False, 
                          instr_bkgnd=False, foreground=False, prng=prng)
 
-    inst = get_instrument_from_registry("hdxi")
-    arf = AuxiliaryResponseFile(inst["arf"])
-    cspec = ConvolvedSpectrum(spec, arf)
-    ph_flux = cspec.get_flux_in_band(0.5, 7.0)[0].value
-    S0 = ph_flux/(np.pi*(r_out**2-r_in**2))
-
     write_radial_profile("ann_evt.fits", "ann_evt_profile.fits", [ra0, dec0],
-                         1.1*r_in, 0.9*r_out, 100, ctr_type="celestial", 
+                         1.1*r_in, 0.9*r_out, 100, ctr_type="celestial",
                          emin=0.5, emax=7.0, overwrite=True)
 
-    load_data(1, "ann_evt_profile.fits", 3, ["RMID","SUR_BRI","SUR_BRI_ERR"])
-    set_stat("chi2")
-    set_method("levmar")
-    set_source("const1d.src")
-    src.c0 = 0.8*S0
-
-    fit()
-    set_covar_opt("sigma", 1.645)
-    covar()
-    res = get_covar_results()
-
-    assert np.abs(res.parvals[0]-S0) < res.parmaxes[0]
+    file_answer_testing("EVENTS", "ann_evt.fits", answer_store)
+    file_answer_testing("PROFILE", "ann_evt_profile.fits", answer_store)
 
     os.chdir(curdir)
     shutil.rmtree(tmpdir)
 
-def test_beta_model():
+
+def test_beta_model(answer_store):
     tmpdir = tempfile.mkdtemp()
     curdir = os.getcwd()
     os.chdir(tmpdir)
@@ -137,38 +121,18 @@ def test_beta_model():
                          "hdxi", [ra0, dec0], ptsrc_bkgnd=False, 
                          instr_bkgnd=False, foreground=False, prng=prng)
 
-    inst = get_instrument_from_registry("hdxi")
-    arf = AuxiliaryResponseFile(inst["arf"])
-    cspec = ConvolvedSpectrum(spec, arf)
-    ph_flux = cspec.get_flux_in_band(0.5, 7.0)[0].value
-    S0 = 3.0*ph_flux/(2.0*np.pi*r_c*r_c)
-
     write_radial_profile("beta_evt.fits", "beta_evt_profile.fits", [ra0, dec0],
                          0.0, 100.0, 200, ctr_type="celestial", emin=0.5, 
                          emax=7.0, overwrite=True)
 
-    load_data(1, "beta_evt_profile.fits", 3, ["RMID","SUR_BRI","SUR_BRI_ERR"])
-    set_stat("chi2")
-    set_method("levmar")
-    set_source("beta1d.src")
-    src.beta = 1.0
-    src.r0 = 10.0
-    src.ampl = 0.8*S0
-    freeze(src.xpos)
-
-    fit()
-    set_covar_opt("sigma", 1.645)
-    covar()
-    res = get_covar_results()
-
-    assert np.abs(res.parvals[0]-r_c) < res.parmaxes[0]
-    assert np.abs(res.parvals[1]-beta) < res.parmaxes[1]
-    assert np.abs(res.parvals[2]-S0) < res.parmaxes[2]
+    file_answer_testing("EVENTS", "beta_evt.fits", answer_store)
+    file_answer_testing("PROFILE", "beta_evt_profile.fits", answer_store)
 
     os.chdir(curdir)
     shutil.rmtree(tmpdir)
 
-def test_beta_model_flux():
+
+def test_beta_model_flux(answer_store):
     tmpdir = tempfile.mkdtemp()
     curdir = os.getcwd()
     os.chdir(tmpdir)
@@ -183,46 +147,32 @@ def test_beta_model_flux():
                                         exp_time, area, prng=prng)
     sim_cat.write_catalog(overwrite=True)
 
-    instrument_simulator("beta_simput.fits", "beta_evt.fits", exp_time,
+    instrument_simulator("beta_simput.fits", "beta_flux_evt.fits", exp_time,
                          "acisi_cy0", [ra0, dec0], ptsrc_bkgnd=False,
                          instr_bkgnd=False, foreground=False, 
                          roll_angle=37.0, prng=prng)
 
-    ph_flux = spec.get_flux_in_band(0.5, 7.0)[0].value
-    S0 = 3.0*ph_flux/(2.0*np.pi*r_c*r_c)
-
     wspec = spec.new_spec_from_band(0.5, 7.0)
 
-    make_exposure_map("beta_evt.fits", "beta_expmap.fits", wspec.emid.value,
+    make_exposure_map("beta_flux_evt.fits", "beta_expmap.fits", wspec.emid.value,
                       weights=wspec.flux.value, overwrite=True)
 
-    write_radial_profile("beta_evt.fits", "beta_evt_profile.fits", [ra0, dec0],
-                         0.0, 100.0, 200, ctr_type="celestial", emin=0.5,
-                         emax=7.0, expmap_file="beta_expmap.fits", overwrite=True)
+    write_radial_profile("beta_flux_evt.fits", "beta_flux_evt_profile.fits",
+                         [ra0, dec0], 0.0, 100.0, 200, ctr_type="celestial",
+                         emin=0.5, emax=7.0, expmap_file="beta_expmap.fits",
+                         overwrite=True)
 
-    load_data(1, "beta_evt_profile.fits", 3, ["RMID","SUR_FLUX","SUR_FLUX_ERR"])
-    set_stat("chi2")
-    set_method("levmar")
-    set_source("beta1d.src")
-    src.beta = 1.0
-    src.r0 = 10.0
-    src.ampl = 0.8*S0
-    freeze(src.xpos)
-
-    fit()
-    set_covar_opt("sigma", 1.645)
-    covar()
-    res = get_covar_results()
-
-    assert np.abs(res.parvals[0]-r_c) < res.parmaxes[0]
-    assert np.abs(res.parvals[1]-beta) < res.parmaxes[1]
-    assert np.abs(res.parvals[2]-S0) < res.parmaxes[2]
+    file_answer_testing("EVENTS", "beta_flux_evt.fits", answer_store)
+    file_answer_testing("PROFILE", "beta_flux_evt_profile.fits", answer_store)
 
     os.chdir(curdir)
     shutil.rmtree(tmpdir)
 
+
 if __name__ == "__main__":
+    import sys
+    answer_store = bool(sys.argv[1])
     test_point_source()
-    test_annulus()
-    test_beta_model()
-    test_beta_model_flux()
+    test_annulus(answer_store)
+    test_beta_model(answer_store)
+    test_beta_model_flux(answer_store)
