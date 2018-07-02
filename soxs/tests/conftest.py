4a5c3c962d6e79d57787fe1704704749332ce736
John ZuHone <jzuhone@gmail.com>
2018-07-02 10:32:25 -0400

    This should be a boolean

--- soxs/tests/conftest.py
+++ soxs/tests/conftest.py
@@ -2,7 +2,7 @@ import pytest
 
 
 def pytest_addoption(parser):
-    parser.addoption("--answer_store",
+    parser.addoption("--answer_store", action="store_true",
                      help="Generate new answers, but don't test. "
                           "Argument is the directory to store the answers to. "
                           "Default: None, which performs the test.")
