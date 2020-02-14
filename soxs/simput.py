67bde97eeadc5b9ed7514f587e9b9cf9abae7c11
John ZuHone <jzuhone@gmail.com>
2020-02-14 16:15:45 -0500

    More cleanup and bug fixing

--- soxs/simput.py
+++ soxs/simput.py
@@ -58,11 +58,10 @@ class SimputCatalog:
         self.emax = ensure_numpy_array(emax)
 
     @classmethod
-    def from_models(cls, src_name, spectral_model, spatial_model,
-                    t_exp, area, prng=None):
+    def from_models(cls, src_names, src_models, t_exp, area, prng=None):
         """
         Generate a SIMPUT catalog object and a single photon list
-        from a spectral and a spatial model. 
+        from a spectral and a spatial model.
 
         Parameters
         ----------
@@ -87,10 +86,18 @@ class SimputCatalog:
             which sets the seed based on the system time.
 
         """
-        ra, dec, e = _e_from_models(prng, t_exp, area, spectral_model, 
-                                    spatial_model)
-        photon_list = SimputPhotonList(src_name, ra, dec, e, e.flux)
-        return cls(photon_list, src_name, e.flux, e.min(), e.max())
+        src_names = ensure_list(src_names)
+        src_models = ensure_list(src_models)
+        sources = []
+        for i, src_name in enumerate(src_names):
+            src = SimputPhotonList.from_models(src_name, src_models[i][0], 
+                                               src_models[i][1], t_exp, 
+                                               area, prng=prng)
+            sources.append(src)
+        return cls(sources, src_names, 
+                   [src.flux for src in sources],
+                   [src.emin for src in sources], 
+                   [src.emax for src in sources])
 
     @classmethod
     def from_file(cls, filename):
@@ -127,8 +134,8 @@ class SimputCatalog:
                 ra = Quantity(data["ra"], "deg")
                 dec = Quantity(data["dec"], "deg")
                 energy = Quantity(data["energy"], "keV")
-                src = SimputPhotonList(src_names[i], ra, dec, energy,
-                                       fluxes[i])
+                src = SimputPhotonList(ra, dec, energy,
+                                       fluxes[i], name=src_names[i])
             sources.append(src)
 
         return cls(sources, src_names, fluxes, e_min, e_max)
@@ -337,9 +344,12 @@ class SimputPhotonList(SimputSource):
             set of random numbers, such as for a test. Default is None, 
             which sets the seed based on the system time. 
         """
-        ra, dec, e = _e_from_models(prng, t_exp, area, spectral_model,
-                                    spatial_model)
-        return cls(name, ra, dec, e, e.flux)
+        prng = parse_prng(prng)
+        t_exp = parse_value(t_exp, "s")
+        area = parse_value(area, "cm**2")
+        e = spectral_model.generate_energies(t_exp, area, prng=prng)
+        ra, dec = spatial_model.generate_coords(e.size, prng=prng)
+        return cls(ra, dec, e, e.flux.value, name=name)
 
     def write_source(self, filename, append=False, overwrite=False):
         """
