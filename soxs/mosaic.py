08e13ee1affb825e30af8a2c0ca1cae709ec0c86
John ZuHone <jzuhone@gmail.com>
2021-04-13 13:46:04 -0400

    Fix up mosaic

--- soxs/mosaic.py
+++ soxs/mosaic.py
@@ -1,16 +1,16 @@
 from soxs.instrument import instrument_simulator
 from soxs.events import write_image, make_exposure_map
-from soxs.utils import parse_value, mylog
+from soxs.utils import mylog
 import numpy as np
 from astropy.io import fits, ascii
-from astropy import wcs
+from astropy.table import Table
 
 
 def make_mosaic_events(pointing_list, input_source, out_prefix, exp_time, 
                        instrument, overwrite=False, instr_bkgnd=True,
                        foreground=True, ptsrc_bkgnd=True, bkgnd_file=None, 
                        no_dither=False, dither_params=None, subpixel_res=False, 
-                       prng=None):
+                       aimpt_shift=None, prng=None):
     """
     Observe a source from many different pointings. 
 
@@ -55,14 +55,21 @@ def make_mosaic_events(pointing_list, input_source, out_prefix, exp_time,
     subpixel_res: boolean, optional
         If True, event positions are not randomized within the pixels 
         within which they are detected. Default: False
+    aimpt_shift : array-like, optional
+        A two-float array-like object which shifts the aimpoint on the 
+        detector from the nominal position. Units are in arcseconds.
+        Default: None, which results in no shift from the nominal aimpoint. 
     prng : :class:`~numpy.random.RandomState` object, integer, or None
         A pseudo-random number generator. Typically will only 
         be specified if you have a reason to generate the same 
         set of random numbers, such as for a test. Default is None, 
         which sets the seed based on the system time. 
     """
-    t = ascii.read(pointing_list, format='commented_header', guess=False,
-                   header_start=0, delimiter="\t")
+    if isinstance(pointing_list, str):
+        t = ascii.read(pointing_list, format='commented_header', guess=False,
+                       header_start=0, delimiter="\t")
+    elif not isinstance(pointing_list, Table):
+        t = Table(np.array(pointing_list), names=["ra", "dec"])
     out_list = []
     for i, row in enumerate(t):
         out_file = f"{out_prefix}_{i}_evt.fits"
@@ -72,7 +79,8 @@ def make_mosaic_events(pointing_list, input_source, out_prefix, exp_time,
                              instr_bkgnd=instr_bkgnd, foreground=foreground,
                              ptsrc_bkgnd=ptsrc_bkgnd, bkgnd_file=bkgnd_file,
                              no_dither=no_dither, dither_params=dither_params,
-                             subpixel_res=subpixel_res, prng=prng)
+                             subpixel_res=subpixel_res, aimpt_shift=aimpt_shift,
+                             prng=prng)
     t["evtfile"] = out_list
     outfile = f"{out_prefix}_event_mosaic.dat"
     mylog.info(f"Writing mosaic information to {outfile}.")
@@ -81,7 +89,7 @@ def make_mosaic_events(pointing_list, input_source, out_prefix, exp_time,
     return outfile
 
 
-def make_mosaic_image(evtfile_list, img_file, emin=None, emax=None,
+def make_mosaic_image(evtfile_list, image_file, emin=None, emax=None,
                       reblock=1, use_expmap=False, expmap_energy=None, 
                       expmap_weights=None, normalize=True, nhistx=16,
                       nhisty=16, overwrite=False):
@@ -95,7 +103,7 @@ def make_mosaic_image(evtfile_list, img_file, emin=None, emax=None,
         The ASCII table produced by :meth:`~soxs.grid.observe_grid_source`
         containing the information about the event files and their
         locations on the sky.
-    img_file : filename
+    image_file : filename
         The name of the FITS image file to be written. This name will
         also be used for the exposure map and flux files if they are
         written.
@@ -124,9 +132,13 @@ def make_mosaic_image(evtfile_list, img_file, emin=None, emax=None,
         Whether or not to overwrite an existing file with the same name.
         Default: False
     """
-    from reproject.mosaicking import find_optimal_celestial_wcs, \
-        reproject_and_coadd
-    from reproject import reproject_interp
+    try:
+        from reproject.mosaicking import find_optimal_celestial_wcs, \
+            reproject_and_coadd
+        from reproject import reproject_interp
+    except ImportError:
+        raise ImportError("The mosaic functionality of SOXS requires the "
+                          "'reproject' package to be installed!")
     t = ascii.read(evtfile_list, format='commented_header',
                    guess=False, header_start=0, delimiter="\t")
 
@@ -135,25 +147,39 @@ def make_mosaic_image(evtfile_list, img_file, emin=None, emax=None,
         evt_file = row["evtfile"]
         img_file = evt_file.replace("evt", "img")
         if use_expmap:
-            expmap_file = evt_file.replace("evt", "expmap")
-            make_exposure_map(evt_file, expmap_file, energy=expmap_energy,
+            emap_file = evt_file.replace("evt", "expmap")
+            make_exposure_map(evt_file, emap_file, energy=expmap_energy,
                               weights=expmap_weights, normalize=normalize,
-                              overwrite=overwrite, reblock=reblock, nhistx=nhistx, 
-                              nhisty=nhisty)
+                              overwrite=overwrite, reblock=reblock, 
+                              nhistx=nhistx, nhisty=nhisty)
         else:
-            expmap_file = None
+            emap_file = None
         write_image(evt_file, img_file, emin=emin, emax=emax,
                     overwrite=overwrite, reblock=reblock)
-        files.append([img_file, expmap_file])
+        files.append([img_file, emap_file])
 
     img_hdus = [fits.open(fns[0], memmap=True)[0] for fns in files]
     wcs_out, shape_out = find_optimal_celestial_wcs(img_hdus)
-
     img, footprint = reproject_and_coadd(img_hdus, wcs_out, shape_out=shape_out,
-                                         reproject_function=reproject_interp)
+                                         reproject_function=reproject_interp,
+                                         combine_function='sum')
+    hdu = fits.PrimaryHDU(img, header=wcs_out.to_header())
+    hdu.writeto(image_file, overwrite=overwrite)
 
     if use_expmap:
         emap_hdus = [fits.open(fns[1], memmap=True)[1] for fns in files]
-        emap, footprint = reproject_and_coadd(emap_hdus, wcs_out, shape_out=shape_out,
-                                              reproject_function=reproject_interp)
+        emap, footprint = reproject_and_coadd(
+            emap_hdus, wcs_out, shape_out=shape_out,
+            reproject_function=reproject_interp, combine_function='sum')
+        hdu = fits.PrimaryHDU(emap, header=wcs_out.to_header())
+        expmap_file = image_file.replace("fits", "expmap")
+        hdu.writeto(expmap_file, overwrite=overwrite)
 
+        with np.errstate(invalid='ignore', divide='ignore'):
+            flux = img / emap
+        flux[np.isinf(flux)] = 0.0
+        flux = np.nan_to_num(flux)
+        flux[flux < 0.0] = 0.0
+        hdu = fits.PrimaryHDU(flux, header=wcs_out.to_header())
+        flux_file = image_file.replace("fits", "flux")
+        hdu.writeto(flux_file, overwrite=overwrite)
