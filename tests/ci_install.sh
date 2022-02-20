9bf5c690329b681528b3ca15c82b98f3db2cac4d
John ZuHone <jzuhone@gmail.com>
2022-02-20 00:09:27 -0500

    Build wheels

--- tests/ci_install.sh
+++ tests/ci_install.sh
@@ -1,22 +1,31 @@
 set -x   # Show which command is being run
 
-# Download answers
+if [[ ${mode} == "testing" ]]; then
 
-wget -q http://hea-www.cfa.harvard.edu/~jzuhone/soxs_test_data_${ANSWER_VER}.tar.gz
-tar -zxf soxs_test_data_${ANSWER_VER}.tar.gz
+  # Download answers
+  
+  wget -q http://hea-www.cfa.harvard.edu/~jzuhone/soxs_test_data_${ANSWER_VER}.tar.gz
+  tar -zxf soxs_test_data_${ANSWER_VER}.tar.gz
+  
+  # Set location of soxs data
+  
+  mkdir -p $HOME/.config/soxs
+  echo "[soxs]" > $HOME/.config/soxs/soxs.cfg
+  echo "soxs_data_dir = \"${PWD}/soxs_data\"" >> $HOME/.config/soxs/soxs.cfg
+  cat $HOME/.config/soxs/soxs.cfg
 
-# Set location of soxs data
-
-mkdir -p $HOME/.config/soxs
-echo "[soxs]" > $HOME/.config/soxs/soxs.cfg
-echo "soxs_data_dir = \"${PWD}/soxs_data\"" >> $HOME/.config/soxs/soxs.cfg
-cat $HOME/.config/soxs/soxs.cfg
+fi 
 
 # Install dependencies using conda and pip
 
 conda install --yes numpy pytest pip astropy scipy cython h5py tqdm pyyaml
 pip install regions
 
-# Install soxs
+if [[ ${mode} == "wheels" ]]; then
+  conda install --yes wheel setuptools
+fi 
 
-python -m pip install -e .
+# Install soxs
+if [[ ${mode} == "testing" ]]; then
+  python -m pip install -e .
+fi
