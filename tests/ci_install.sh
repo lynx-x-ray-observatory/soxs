62c4de2e1711fbe1f85121a8f67991a1841ba29f
John ZuHone <jzuhone@gmail.com>
2023-03-05 08:52:08 -0500

    platform-independence

--- tests/ci_install.sh
+++ tests/ci_install.sh
@@ -11,8 +11,8 @@ if [[ ${mode} == "testing" ]]; then
 
   mkdir -p $HOME/.config/soxs
   echo "[soxs]" > $HOME/.config/soxs/soxs.cfg
-  echo "soxs_data_dir = ${PWD}/soxs_data" >> $HOME/.config/soxs/soxs.cfg
-  echo "soxs_answer_dir = ${PWD}/soxs_test_data" >> $HOME/.config/soxs/soxs.cfg
+  echo "soxs_data_dir = ${GITHUB_WORKSPACE}/soxs_data" >> $HOME/.config/soxs/soxs.cfg
+  echo "soxs_answer_dir = ${GITHUB_WORKSPACE}/soxs_test_data" >> $HOME/.config/soxs/soxs.cfg
   cat $HOME/.config/soxs/soxs.cfg
 
 fi
