6924d9389f1a6179be9d68ac29cc199e07f2c5ae
John ZuHone <jzuhone@gmail.com>
2024-06-01 13:23:46 -0400

    Fix import bugs

--- soxs/spectra/__init__.py
+++ soxs/spectra/__init__.py
@@ -1,4 +1,12 @@
-from .base import ConvolvedSpectrum, CountRateSpectrum, Spectrum
+from .base import (
+    ConvolvedSpectrum,
+    CountRateSpectrum,
+    Energies,
+    Spectrum,
+    _generate_energies,
+    get_tbabs_absorb,
+    get_wabs_absorb,
+)
 from .charge_exchange import ChargeExchangeGenerator
 from .thermal_spectra import (
     ApecGenerator,
