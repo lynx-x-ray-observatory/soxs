11c3d49c569ac9f2a3fcb8005c76f578820a7be8
John ZuHone <jzuhone@gmail.com>
2025-12-09 17:03:50 -0500

    Forgot to drop units

--- soxs/tests/test_xstar.py
+++ soxs/tests/test_xstar.py
@@ -9,5 +9,5 @@ def test_xstar():
     answer_dir = Path(soxs_cfg.get("soxs", "soxs_answer_dir"))
     spec = CountRateSpectrum.from_xstar_model(answer_dir / "xout_spect1.fits", "emit_outward")
     rate, lum = spec.get_rate_in_band(0.5, 7.0)
-    assert_allclose(rate, 4.04361905e54)
-    assert_allclose(lum, 3.77093392e45)
+    assert_allclose(rate.value, 4.04361905e54)
+    assert_allclose(lum.value, 3.77093392e45)
